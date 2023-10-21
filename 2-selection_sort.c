92d0a92155ddc7b927a4a25a3516c014bfdd92ce
Teeclever <omotoboraomotayo@gmail.com>
2023-10-21 13:49:19 -0700

    selction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,7 +12,6 @@ void selection_sort(int *array, size_t size)
 	unsigned int i, j;
 	int temp;
 	unsigned int idx = 0;
-	bool check;
 
 	if (array == NULL || size < 2)
 		return;
@@ -20,20 +19,17 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		idx = i;
-		check = false;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[idx])
-			{
 				idx = j;
-				check = true;
-			}
 		}
-
-		temp = array[i];
-		array[i] = array[idx];
-		array[idx] = temp;
-		if (check)
+		if (idx != 1)
+		{
+			temp = array[i];
+			array[i] = array[idx];
+			array[idx] = temp;
 			print_array(array, size);
+		}
 	}
 }
