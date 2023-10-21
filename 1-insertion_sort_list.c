c0095a582fc99fe0d1536ee5ab302f48f6179b47
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-21 19:20:59 +0100

    Alx

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,21 +9,4 @@ void insertion_sort_list(listint_t **list)
 	size_t a, i;
 	int swap, temp;
 
-	for (a = 0; a < (size - 1); a++)
-	{
-		swap = 0;
-		for (i = 0; i < (size - a - 1); i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				swap = 1;
-			}
-		}
-		print_array(array, size);
-		if (swap == 0)
-			return;
-	}
 }
