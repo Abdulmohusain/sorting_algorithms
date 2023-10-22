c26a1dac3ee4ff0fbe52c7db323c39758a1ac71f
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-22 16:43:40 +0100

    Alx

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,12 +1,40 @@
 #include "sort.h"
 /**
- * bubble_sort - function that sorts a doubly linked list of
+ * insertion_sort_list - function that sorts a doubly linked list of
  * integers in ascending order using the Insertion sort algorithm
  * @list: the list
  */
 void insertion_sort_list(listint_t **list)
 {
-	size_t a, i;
-	int swap, temp;
+	listint_t *list_copy, *current, *temp, *next;
+	int i = 0;
 
+	if (*list == NULL)
+		return;
+	list_copy = (*list)->next;
+	while (list_copy)
+	{
+		next = list_copy->next;
+		current = list_copy;
+		i++;
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			if (current->prev)
+			{
+			if (current->next)
+				current->next->prev = current->prev;
+			if (current->prev->prev)
+				current->prev->prev->next = current;
+			temp = current->prev->prev;
+			current->prev->prev = current;
+			current->prev->next = current->next;
+			current->next = current->prev;
+			current->prev = temp;
+			if (current->prev == NULL)
+				*list = current;
+			print_list(*list);
+			}
+		}
+	list_copy = next;
+	}
 }
